e547f2903ec9748932a8d5c17025ac49c46e6e34
Johnson Wang <yuhang0229@gmail.com>
2025-05-31 14:11:44 +1000

    messages and chat aggregation

--- chatter-backend/src/chats/messages/messages.resolver.ts
+++ chatter-backend/src/chats/messages/messages.resolver.ts
@@ -1,21 +1,19 @@
 import { Args, Mutation, Query, Resolver, Subscription } from '@nestjs/graphql';
 import { MessagesService } from './messages.service';
 import { Message } from './entities/message.entity';
-import { Inject, UseGuards } from '@nestjs/common';
+import { UseGuards } from '@nestjs/common';
 import { GqlAuthGuard } from 'src/auth/guards/gql-auth.guard';
 import { CreateMessageInput } from './dto/create-message.input';
 import { CurrentUser } from 'src/auth/current-user-decorator';
 import { TokenPayload } from 'src/auth/token-payload.interface';
 import { GetMessageArgs } from './dto/get-message.args';
-import { PUB_SUB } from 'src/common/constants/injection-token';
-import { PubSub } from 'graphql-subscriptions';
+
 import { MessageCreatedArgs } from './dto/message-create.args';
 
 @Resolver(() => Message)
 export class MessagesResolver {
   constructor(
     private readonly messagesService: MessagesService,
-    @Inject(PUB_SUB) private pubSub: PubSub
   ) { }
 
   @Mutation(() => Message)
@@ -23,7 +21,7 @@ export class MessagesResolver {
   async createMessage(
     @Args('createMessageInput') createMessageInput: CreateMessageInput,
     @CurrentUser() user: TokenPayload
-  ) {
+  ): Promise<Message> {
     return this.messagesService.createMessage(createMessageInput, user._id)
   }
 
@@ -31,9 +29,8 @@ export class MessagesResolver {
   @UseGuards(GqlAuthGuard)
   async getMessages(
     @Args() getMessageArgs: GetMessageArgs,
-    @CurrentUser() user: TokenPayload,
-  ) {
-    return this.messagesService.getMessages(getMessageArgs, user._id)
+  ): Promise<Message[]> {
+    return this.messagesService.getMessages(getMessageArgs)
   }
 
 
@@ -41,13 +38,17 @@ export class MessagesResolver {
     filter: (payload, variables, context) => {
       // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
       const userId = context.req.user._id;
-      return (payload.messageCreated.chatId === variables.chatId && userId !== payload.messageCreated.userId);
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      const message: Message = payload.messageCreated;
+      return (
+        payload.messageCreated.chatId === variables.chatId &&
+        userId !== message.user._id.toHexString()
+      );
     }
   })
   messageCreated(
     @Args() messageCreatedArgs: MessageCreatedArgs,
-    @CurrentUser() user: TokenPayload
   ) {
-    return this.messagesService.messageCreated(messageCreatedArgs, user._id);
+    return this.messagesService.messageCreated(messageCreatedArgs);
   }
 }
