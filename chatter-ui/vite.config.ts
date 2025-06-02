7ef7e05e6b94a420908e59a273ef1611f99cf98c
Johnson Wang <yuhang0229@gmail.com>
2025-06-02 22:07:16 +1000

    test

--- chatter-ui/vite.config.ts
+++ chatter-ui/vite.config.ts
@@ -1,21 +1,16 @@
-import { defineConfig, type ConfigEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 
 
-export default ({ mode }: ConfigEnv) => {
-  const typedMode = mode as 'development' | 'production'
+export default () => {
 
-  const backendUrls = {
-    development: 'http://localhost:3000',
-    production: 'https://prod.eba-xbaeycjv.ap-southeast-2.elasticbeanstalk.com',
-  }
   return defineConfig({
     plugins: [react()],
     server: {
       proxy: {
         '/api': {
-          target: backendUrls[typedMode] || backendUrls.development,  // now you get the env var correctly
+          target: "https://prod.eba-xbaeycjv.ap-southeast-2.elasticbeanstalk.com",
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/api/, ''),
         },
