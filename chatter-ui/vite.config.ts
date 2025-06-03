b52b0c84dd150f447d708989ee6a38db1c4d7bf8
Johnson Wang <yuhang0229@gmail.com>
2025-06-03 21:09:03 +1000

    test proxy

--- chatter-ui/vite.config.ts
+++ chatter-ui/vite.config.ts
@@ -1,21 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
-
-export default () => {
-
-  return defineConfig({
-    plugins: [react()],
-    server: {
-      proxy: {
-        '/api': {
-          target: "https://prod.eba-xbaeycjv.ap-southeast-2.elasticbeanstalk.com",
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, ''),
-          secure: false,
-        },
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
-  })
-}
+  },
+})
