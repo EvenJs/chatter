643efc60a8e98a1861ae52d98abb310816d979dc
Johnson Wang <yuhang0229@gmail.com>
2025-06-04 14:29:08 +1000

    test

--- chatter-ui/vite.config.ts
+++ chatter-ui/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://prod.eba-xbaeycjv.ap-southeast-2.elasticbeanstalk.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
